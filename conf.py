7eb693fb22e15708d62a75e3049ee57fc77e4090
Taichi Uragami <backpaper0@gmail.com>
2015-01-14 23:44:42 +0900

    テーマをflatに戻した
    iPhoneで見たときに見辛さ絶望級だったので。

--- conf.py
+++ conf.py
@@ -33,7 +33,7 @@ disqus_shortname = None
 html_favicon = 'backpaper0.ico'           
 
 # Pick another Tinkerer theme or use your own
-html_theme = "minimal5"
+html_theme = "flat"
 
 # Theme-specific options, see docs
 html_theme_options = { }                                  
