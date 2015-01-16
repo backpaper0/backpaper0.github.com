a1759df0e5a067d3ea40aaa63fc0557a9c119c47
Taichi Uragami <backpaper0@gmail.com>
2015-01-17 07:19:45 +0900

    テーマを sphinxjp.themes.tinkeralizarin に変更した。
    https://pypi.python.org/pypi/sphinxjp.themes.tinkeralizarin/0.1.0

--- conf.py
+++ conf.py
@@ -33,7 +33,7 @@ disqus_shortname = None
 html_favicon = 'backpaper0.ico'           
 
 # Pick another Tinkerer theme or use your own
-html_theme = "flat"
+html_theme = "tinkerbelizehole"
 
 # Theme-specific options, see docs
 html_theme_options = { }                                  
@@ -50,7 +50,7 @@ posts_per_page = 2
 # **************************************************************
 
 # Add other Sphinx extensions here
-extensions = ['tinkerer.ext.blog', 'tinkerer.ext.disqus'] 
+extensions = ['tinkerer.ext.blog', 'tinkerer.ext.disqus', 'sphinxjp.themecore'] 
 
 # Add other template paths here
 templates_path = ['_templates']
